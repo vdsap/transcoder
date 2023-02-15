65b1e5216f956c70b5252bf4af1d281d8fd4f6c9
vdsap <vadim18022004@gmail.com>
2023-02-15 18:18:35 +0300

    removed temp directories

--- pyinstaller.py
+++ pyinstaller.py
@@ -1,4 +1,6 @@
 import PyInstaller.__main__
+import shutil
+import os
 
 PyInstaller.__main__.run([
     'main.py',
@@ -6,3 +8,7 @@ PyInstaller.__main__.run([
     '-iicon.ico',
     '-n Transcoder',
 ])
+shutil.copyfile('dist/ Transcoder.exe','./Transcoder.exe')
+shutil.rmtree('build')
+shutil.rmtree('dist')
+os.remove(' Transcoder.spec')
