2a02a2a4a66cebba551f2365e563953b5207f433
vdsap <vadim18022004@gmail.com>
2023-02-15 19:22:35 +0300

    added logging

--- pyinstaller.py
+++ pyinstaller.py
@@ -1,14 +1,21 @@
 import PyInstaller.__main__
 import shutil
 import os
+from loguru import logger
 
+logger.info('Compilling started')
 PyInstaller.__main__.run([
     'main.py',
     '--onefile',
     '-iicon.ico',
     '-n Transcoder',
 ])
+logger.info('Compilling finished')
 shutil.copyfile('dist/ Transcoder.exe','./Transcoder.exe')
+logger.info('Copying finished')
 shutil.rmtree('build')
+logger.info('Removing build folder')
 shutil.rmtree('dist')
+logger.info('Removing dist folder')
 os.remove(' Transcoder.spec')
+logger.info('Removing spec file')
