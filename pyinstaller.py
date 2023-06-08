69184c8e5c41eddac0ec76aeca521efed85b4c58
vdsap <vadim18022004@gmail.com>
2023-06-08 20:09:26 +0300

    some code refactor

--- pyinstaller.py
+++ pyinstaller.py
@@ -3,19 +3,20 @@ import shutil
 import os
 from loguru import logger
 
-logger.info('Compilling started')
+logger.info('Compiling started')
 try:
     os.remove('Transcoder.zip')
     logger.info('zip deleted')
-except: pass
+except:
+    print('Previous file not found')
 PyInstaller.__main__.run([
     'main.py',
     '--onefile',
     '-iicon.ico',
     '-n Transcoder',
 ])
-logger.info('Compilling finished')
-shutil.copyfile('dist/ Transcoder.exe','./Transcoder.exe')
+logger.info('Compiling finished')
+shutil.copyfile('dist/ Transcoder.exe', './Transcoder.exe')
 logger.info('Copying finished')
 shutil.rmtree('build')
 logger.info('Removing build folder')
@@ -26,15 +27,16 @@ logger.info('Removing spec file')
 try:
     os.mkdir('release')
     os.mkdir('release/Transcoder')
-except: pass
+except:
+    pass
 logger.info('Created release directory')
 shutil.copyfile('Transcoder.exe', 'release/Transcoder/Transcoder.exe')
 logger.info('Transcoder.exe copied')
 shutil.copyfile('ffmpeg.exe', 'release/Transcoder/ffmpeg.exe')
 logger.info('Ffmpeg.exe copied')
 logger.info('Creating zip')
-shutil.make_archive('Transcoder','zip', 'release')
+shutil.make_archive('Transcoder', 'zip', 'release')
 logger.info('Archive made')
 shutil.rmtree('release')
 logger.info('Release directory removed')
-logger.info('Compilling compleate')
+logger.info('Compiling complete')
